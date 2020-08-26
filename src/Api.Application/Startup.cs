4dc3ab0097943366e4cd07100be4c32b53bd6337
Willian Borges <willianmarcel@gmail.com>
2020-08-26 16:29:44 -0300

    Logica de Autenticação e Geração do Token JWT

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
+using Api.Domain.Security;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace application
@@ -26,10 +28,22 @@ namespace application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Injeção de dependencia
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
+            //Token de autenticação
+            var signingConfigurations = new SigningConfigurations();
+            services.AddSingleton(signingConfigurations);
+
+            var tokenConfigurations = new TokenConfigurations();
+            new ConfigureFromConfigurationOptions<TokenConfigurations>(Configuration.GetSection("TokenConfigurations")).Configure(tokenConfigurations);
+            services.AddSingleton(tokenConfigurations);
+
+            //Controllers MVC
             services.AddControllers();
+
+            //swagger
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
--- src/Api.Service/Services/LoginService.cs
+++ src/Api.Service/Services/LoginService.cs
@@ -1,25 +1,99 @@
+using System;
+using System.Security.Principal;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Api.Domain.Dtos;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Repository;
 using Api.Domain.Interfaces.Services.Users;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Services
 {
     public class LoginService : ILoginService
     {
         private readonly IUserRepository _repository;
+        private SigningConfigurations _signingConfigurations;
+        private TokenConfigurations _tokenConfigurations;
+        private IConfiguration _configuration{ get; }
 
-        public LoginService(IUserRepository repository)
+        public LoginService(
+            IUserRepository repository, 
+            SigningConfigurations signingConfigurations, 
+            TokenConfigurations tokenConfigurations,
+            IConfiguration configuration)
         {
             this._repository = repository;
+            this._signingConfigurations = signingConfigurations;
+            this._tokenConfigurations = tokenConfigurations;
+            this._configuration = configuration;
         }
 
         public async Task<object> FindByLogin(LoginDto login)
         {
+            var baseUser = new UserEntity();
+
             if(login != null && !string.IsNullOrWhiteSpace(login.Email))
-                return await _repository.FindByLogin(login.Email);
+            {
+                baseUser = await _repository.FindByLogin(login.Email);
+
+                if(baseUser == null)
+                    return new { 
+                        authenticated = false,
+                        message = "Falha ao autenticar"
+                    };
+                
+                var identity = new ClaimsIdentity(
+                    new GenericIdentity(baseUser.Email),
+                    new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, login.Email),
+                    }
+                );
+
+                var createDate = DateTime.Now;
+                var expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var token = CreateToken(identity, createDate, expirationDate, handler);
+
+                return SuccessObject(createDate, expirationDate, token, login);
+            }
+                
             
             return null;
+        }
+
+        private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
+        {
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _tokenConfigurations.Issuer,
+                Audience = _tokenConfigurations.Audience,
+                SigningCredentials = _signingConfigurations.SigningCredentials,
+                Subject = identity,
+                NotBefore = createDate,
+                Expires = expirationDate,
+            });
+
+            var token = handler.WriteToken(securityToken);
+            return token;
+        }
+
+        private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, LoginDto user)
+        {
+            return new {
+                authenticated = true,
+                created = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                expiration = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                accessToken = token,
+                userName = user.Email,
+                message = "Usuário logado com sucesso"
+            };
         }
     }
 }
