0b3851d4a3508f1e7d538f90944b3a26fec2e654
Willian Borges <willianmarcel@gmail.com>
2020-08-26 12:58:02 -0300

    Inclusão de Middleware swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -29,6 +30,26 @@ namespace application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Api Modelo Asp.Net Core 3.1",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://www.wmtecnologia.com.br"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Willian Marcel Borges",
+                        Email = "willianmarcel@gmail.com",
+                        Url = new Uri("http://www.wmtecnologia.com.br")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termos de Licensa de Uso",
+                        Url = new Uri("http://www.wmtecnologia.com.br")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +59,13 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Modelo com Asp.Net Core 3.1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
